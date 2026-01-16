6e2ac5d8dfb49785e673a71b65b1abd7124e971d
humzhumz3-lgtm <humzhumz3@gmail.com>
2026-01-16 13:06:20 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,9 +1,11 @@
-local HttpService = game:GetService("HttpService")
-
 local payload = game:HttpGet(
     "https://raw.githubusercontent.com/humzhumz3-lgtm/sentinel---loader/main/payload.txt",
     true
 )
 
-local decoded = HttpService:Base64Decode(payload)
-loadstring(decoded)()
+if not payload or payload == "" then
+    warn("Failed to load payload")
+    return
+end
+
+loadstring(payload)()
